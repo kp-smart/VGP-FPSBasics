350639f4aec63f22e689609c3a9b584370b6d06d
kp-smart <palaganaskalel@gmail.com>
2026-02-21 15:11:34 -0800

    Bullets can now destroy enemies

--- Assets/Scripts/Bullets/Destroy.cs
+++ Assets/Scripts/Bullets/Destroy.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float bulletLifespan = 5.0f;
+
+    // Awake is called when the gameObject is called
+    void Awake()
     {
-        
+        Destroy(gameObject, bulletLifespan);
+        //destroys bullet after 5 seconds
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.SetActive(false);
+            //destroys obstacle
+
+            Destroy(gameObject);
+            //destroys bullet
+        }
+
     }
 }
--- Assets/Scripts/Player/Collecting.cs
+++ Assets/Scripts/Player/Collecting.cs
@@ -6,12 +6,9 @@ public class Collecting : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Hit!");
-
         if(other.gameObject.tag == "Coin")
         {
             Destroy(other.gameObject);
-            Debug.Log("Coin get!");
         }
     }
 }
