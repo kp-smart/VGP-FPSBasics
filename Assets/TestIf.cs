9847704adbaee41ff32cb1d003f94913cdb55277
kp-smart <palaganaskalel@gmail.com>
2026-03-14 13:49:07 -0700

    Added AI nav to enemies

---
+++ Assets/TestIf.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestIf : MonoBehaviour
+{
+    public int age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!" + age);
+    }
+}
